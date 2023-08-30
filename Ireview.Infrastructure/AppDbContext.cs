b2c20c29a747e864b47334904ed5bd18d3cf735a
Nikita Kyteyko <nikitakuteyko@gmail.com>
2023-08-30 18:49:28 +0300

    Adding DbContext, Models and edited Layout

---
+++ Ireview.Infrastructure/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Ireview.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ireview.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Article> Articles;
+        public DbSet<Tag> Tags;
+    }
+}
