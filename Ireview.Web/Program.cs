12067d5c7d2c729fe6ff5612ca150ac6c1008dc1
Nikita Kyteyko <nikitakuteyko@gmail.com>
2023-09-21 08:05:37 +0300

    Editing Program.cs

--- Ireview.Web/Program.cs
+++ Ireview.Web/Program.cs
@@ -14,6 +14,7 @@ using Ireview.Web.Configuration;
 using Ireview.Web.Interfaces;
 using System.Drawing;
 using Ireview.Web.Services;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -59,6 +60,11 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto
+});
 
 app.UseDeveloperExceptionPage();
 app.UseDatabaseErrorPage();
